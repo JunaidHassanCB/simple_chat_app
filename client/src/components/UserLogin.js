e3b91859213b6f09637ee8d66bd9d4008e31be80
JunaidHassanCB <137034388+JunaidHassanCB@users.noreply.github.com>
2024-05-07 15:38:45 +0500

    feat: ready for our use

--- client/src/components/UserLogin.js
+++ client/src/components/UserLogin.js
@@ -1,53 +1,119 @@
-import React, { useState } from 'react'
-import { CommentOutlined } from '@ant-design/icons'
-import _ from 'lodash'
+import React, { useState } from "react";
+import { CommentOutlined } from "@ant-design/icons";
+import _ from "lodash";
 
 const button = {
-    width:'10%' ,
-    height:50 ,
-    fontWeight:'bold', 
-    borderRadius:10 ,
-    fontSize:18 ,
-    backgroundColor:'#075e54',
-    borderWidth:0,
-    color:'#fff',
-    margin:10
-  }
+  width: "10%",
+  height: 50,
+  fontWeight: "bold",
+  borderRadius: 10,
+  fontSize: 18,
+  backgroundColor: "#075e54",
+  borderWidth: 0,
+  color: "#fff",
+  margin: 10,
+};
 
+export default function UserLogin({ setUser }) {
+  const [user, setAUser] = useState({
+    senderPhone: "15556116462",
+    receiverPhone: "",
+    userName: "Test User",
+  });
 
-export default function UserLogin({setUser}) {
+  function handleSetUser() {
+    console.log(user);
 
-    const [user , setAUser] = useState("")
+    if (!user) return;
 
-    function handleSetUser(){
-        if(!user) return
-        localStorage.setItem("user" , user)
-        setUser(user)
-        localStorage.setItem("avatar" ,`https://picsum.photos/id/${_.random(1, 1000)}/200/300`)
+    localStorage.setItem("user", user);
     
-    }
+    setUser(user);
+    localStorage.setItem(
+      "avatar",
+      `https://picsum.photos/id/${_.random(1, 1000)}/200/300`
+    );
+  }
 
   return (
     <div>
-          <h1 style={{margin:10, textAlign:'center'}}><CommentOutlined color={'green'} />  Super Chat </h1>
-          
-          <div style={{display:'flex' ,justifyContent:'center'}}>
+      <h1 style={{ margin: 10, textAlign: "center" }}>
+        <CommentOutlined color={"green"} /> Super Chat{" "}
+      </h1>
 
-          <input
-                    style={{margin:10, height:30, width:'25%', borderRadius:10 , borderWidth:10 , fontSize:15 , paddingInline:5}}
-                    value={user}
-                    onChange={e=> setAUser(e.target.value)}
-                    placeholder="Write a random name"
-                    ></input>
-        <button
-                 onClick={()=> handleSetUser()}
-                 style={button}
-                >
-                    Login
-                </button>
+      <div
+        style={{
+          display: "flex",
+          flexDirection: "column",
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+      >
+        <input
+          disabled={true}
+          style={{
+            margin: 10,
+            height: 30,
+            width: "25%",
+            borderRadius: 10,
+            borderWidth: 10,
+            fontSize: 15,
+            paddingInline: 5,
+          }}
+          value={user.userName}
+          onChange={(e) =>
+            setAUser((prev) => {
+              return { ...prev, userName: e.target.value };
+            })
+          }
+          placeholder="User Name"
+        ></input>
 
-          </div>
-          
+        <input
+          disabled={true}
+          style={{
+            margin: 10,
+            height: 30,
+            width: "25%",
+            borderRadius: 10,
+            borderWidth: 10,
+            fontSize: 15,
+            paddingInline: 5,
+          }}
+          value={user.senderPhone}
+          onChange={(e) =>
+            setAUser((prev) => {
+              return { ...prev, senderPhone: e.target.value };
+            })
+          }
+          placeholder="Sender Phone Number"
+        ></input>
+
+        <input
+          style={{
+            margin: 10,
+            height: 30,
+            width: "25%",
+            borderRadius: 10,
+            borderWidth: 10,
+            fontSize: 15,
+            paddingInline: 5,
+          }}
+          value={user.receiverPhone}
+          onChange={(e) =>
+            setAUser((prev) => {
+              return { ...prev, receiverPhone: e.target.value };
+            })
+          }
+          placeholder="Receiver Phone Number"
+        ></input>
+      </div>
+
+      <div style={{ display: "flex", justifyContent: "center" }}>
+        <button onClick={() => handleSetUser()} style={button}>
+          Login
+        </button>
+      </div>
     </div>
-  )
+  );
 }
