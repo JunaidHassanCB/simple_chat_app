0cffbbdc2e6b3e1ea78d052e9a436b95a4bb30f1
JunaidHassanCB <137034388+JunaidHassanCB@users.noreply.github.com>
2024-05-09 16:18:11 +0500

    feat: backend ready for config server and receiving messages

--- server/app.js
+++ server/app.js
@@ -1,19 +1,63 @@
 const express = require("express");
-const app = express();
-const port = 3001;
+const bodyParser = require("body-parser");
 
+const app = express();
+
+// Tell express to use the body-parser middleware and to not parse extended bodies
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+var customParser = bodyParser.json({
+  type: function (req) {
+    if (req.headers["content-type"] === "") {
+      return (req.headers["content-type"] = "application/json");
+    } else if (typeof req.headers["content-type"] === "undefined") {
+      return (req.headers["content-type"] = "application/json");
+    } else {
+      return (req.headers["content-type"] = "application/json");
+    }
+  },
+});
+
+app.use(
+  bodyParser.json({
+    limit: "50mb",
+    extended: true,
+  })
+); // support encoded bodies
+
+app.use(
+  bodyParser.urlencoded({
+    limit: "50mb",
+    extended: true,
+  })
+); // support encoded bodies
+
+// global variables
 let messages = [];
 
-app.get("/webhook/messages", (req, res) => {
-  console.log(`messages: ${Json.stringify(messages)}`);
+app.get("/", (req, res) => {
+  res.status(200).send("Server is up and running");
+});
 
+app.get("/messages", (req, res) => {
   res.status(200).json(messages);
 });
 
-// should also try post and get
-app.post("/webhook/listener", (req, res) => {
-  // explaination
-  // https://developers.facebook.com/docs/messenger-platform/getting-started/webhook-setup/
+// This runs when an event happens like user sends message
+app.post("/webhook", customParser, (req, res) => {
+  console.log(`post => /webhook`);
+
+  const body = req.body;
+  messages.push(body);
+
+  res.status(200);
+});
+
+// This only works once, when we are initializing the server
+app.get("/webhook", (req, res) => {
+  console.log("get => /webhook/listener");
+
   const mode = req.query["hub.mode"];
   const challenge = req.query["hub.challenge"];
   const token = req.query["hub.verify_token"];
@@ -22,17 +66,27 @@ app.post("/webhook/listener", (req, res) => {
     if (mode === "subscribe" && token) {
       console.log("WEBHOOK_VERIFIED");
 
-      // res.status(200).send(challenge);
-      console.log(`body: ${Json.stringify(req.body)}`);
-
-      messages.push(req.body);
-      res.status(200).send("Done");
+      // we must send this response otherwise the request will not be validated
+      res.status(200).send(challenge);
     } else {
       res.sendStatus(403);
     }
   }
 });
 
+const port = process.env.PORT || 3001;
+
 app.listen(port, () => {
   console.log(`App is running at http://localhost:${port}`);
+
+  const ngrok = require("ngrok");
+
+  ngrok
+    .connect(port)
+    .then((ngrokUrl) => {
+      console.log(`connected at port ${port} with ngrokUrl ${ngrokUrl}`);
+    })
+    .catch((error) => {
+      console.log(`could not connect to the tunnel ${error}`);
+    });
 });
